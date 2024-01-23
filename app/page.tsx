5c671ad2d9f33f1e69198157006596da6861af01
krisz <devkapsakrisz@gmail.com>
2024-01-23 21:00:02 +0000

    gif problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import DemoGif from "@/images/landingPage/demo.gif";
+import DemoGif from "/images/landingPage/demo.gif";
 import Image from "next/image";
 
 export default function HomePage() {
@@ -23,7 +23,7 @@ export default function HomePage() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-                Chat with Anyone, anywhere!
+                Chat with Anyone, anywhereaaa!
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
                 You speak your language, they speak their language.{" "}
